77fd814c266ce5e3cca4a261d8446862bd6fced1
Xyckno <Xyckno@gmail.com>
2020-10-13 11:47:48 +0200

    added player movement and virtual PlayerCam

---
+++ Assets/Scripts/GameStateMachine.cs
@@ -0,0 +1,31 @@
+﻿﻿public class GameStateMachine
+{
+    private static GameStateMachine _instance;
+    private State _currentState = State.Playing;
+
+    public static GameStateMachine GetInstance()
+    {
+        return _instance ?? (_instance = new GameStateMachine());
+    }
+
+    public enum State
+    {
+        Playing,
+        Paused,
+        MainMenu,
+    }
+
+    public State GetState()
+    {
+        return _currentState;
+    }
+
+    public void SetState(State state)
+    {
+        if (_currentState != state)
+        {
+            this._currentState = state;
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody playerBody;
+    [SerializeField] private int moveSpeed = 20;    
+    // [SerializeField] private Game game;
+    
+    private Vector3 _inputVector;
 
-    // Update is called once per frame
     void Update()
     {
-        //hello
+        // Game is not playing, nothing to do
+        if (GameStateMachine.GetInstance().GetState() != GameStateMachine.State.Playing) return;
+        
+        _inputVector = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, playerBody.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        transform.LookAt(transform.position + new Vector3(_inputVector.x, 0, _inputVector.z));
+    }
+
+    private void FixedUpdate()
+    {
+        playerBody.velocity = _inputVector;
     }
 }
