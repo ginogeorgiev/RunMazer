90bbb4b75c05688ea5005676f8c54d21ff76ac04
Xyckno <Xyckno@gmail.com>
2020-10-13 21:59:58 +0200

    added Hub and working HungerBar and PlayerSates edit cam pos

---
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -0,0 +1,30 @@
+﻿public class PlayerStateMachine
+{
+    private static PlayerStateMachine instance;
+    private State currentState = State.IsEating;
+
+    public static PlayerStateMachine GetInstance()
+    {
+        return instance ?? (instance = new PlayerStateMachine());
+    }
+
+    public enum State
+    {
+        HasHunger,
+        IsEating,
+    }
+
+    public State GetState()
+    {
+        return currentState;
+    }
+
+    public void SetState(State state)
+    {
+        if (currentState != state)
+        {
+            this.currentState = state;
+        }
+    }
+
+}
