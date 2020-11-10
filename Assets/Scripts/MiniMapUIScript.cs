05bdcb604718183a9be31ded0b76c414e1ad0df3
Joey <jonathan.jander@gmx.de>
2020-11-10 14:15:01 +0100

    added anchor position variable

--- Assets/Scripts/MiniMapUIScript.cs
+++ Assets/Scripts/MiniMapUIScript.cs
@@ -6,12 +6,14 @@ public class MiniMapUIScript : MonoBehaviour
 {
     private RectTransform trans;
     private bool isMaximized;
+    private Vector2 defaultAnchoredPos;
     void Start()
     {
         trans = (RectTransform)gameObject.transform;
         isMaximized = false;
+        defaultAnchoredPos = trans.anchoredPosition;
+        
     }
-    
     // Update is called once per frame
     void Update()
     {
@@ -25,14 +27,13 @@ public class MiniMapUIScript : MonoBehaviour
                 trans.localPosition = new Vector3(0.5f, 0.5f);
                 //scale it up to the height of our screen
                 trans.localScale = new Vector3(scaleVal, scaleVal);
-                
                 isMaximized = true;
             }
             else
             {
                 trans.localScale = new Vector3(1.0f,1.0f);
-                //default position of the minimap relative to the Canvas
-                trans.anchoredPosition = new Vector2(-172f,172f);
+                //default position of the minimap relative to the Canvas and the anchor (bottom right)
+                trans.anchoredPosition = defaultAnchoredPos;
                 isMaximized = false;
             }
 
