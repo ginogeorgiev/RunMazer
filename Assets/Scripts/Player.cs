696d3525dd0b336e086f4fcb4c5ff5898c1cc3fd
Jonas <jonas.endert@web.de>
2020-11-27 12:45:50 +0100

    deleted everything connected to caffeineRush state

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,13 +21,8 @@ public class Player : MonoBehaviour
     // [SerializeField] private Game game;
 
     [SerializeField] private int moveSpeedWalking = 15;
-    [SerializeField] private int moveSpeedRunning = 20;
-    [SerializeField] private int moveSpeedCaffeine = 25;
-
-    //time spent in caffeineRush state in s
-    [SerializeField] private float stateChangeDelay = 5f;
-
-    private bool isInOtherState = false;
+    [SerializeField] private int moveSpeedRunning = 2;
+    
     private static bool isInBase;
 
     private Vector3 inputVector;
@@ -50,17 +45,6 @@ public class Player : MonoBehaviour
 
         switch (playerState)
         {
-            case PlayerStateMachine.State.CaffeineRush:
-                timeInState += Time.deltaTime;
-
-                moveSpeed = moveSpeedCaffeine;
-
-                //Delays change into basic states (Idle, Walking, Running) by given time in s
-                this.StateChangeDelay(stateChangeDelay);
-                
-                break;
-
-
             case PlayerStateMachine.State.IsWalking:
 
                 moveSpeed = moveSpeedWalking;
@@ -120,8 +104,6 @@ public class Player : MonoBehaviour
 
     private void DetermineState()
     {
-        if (isInOtherState) return;
-        
         //player isn't moving
         if (inputVector == Vector3.zero)
         {
@@ -141,17 +123,5 @@ public class Player : MonoBehaviour
         //player is moving, shift is held down and stamina isn't depleted
         PlayerStateMachine.GetInstance().ChangeState(PlayerStateMachine.State.IsRunning);
         
-    }
-
-    private void StateChangeDelay(float delay)
-    {
-        if (timeInState > delay)
-        {
-            timeInState = 0;
-
-            isInOtherState = false;
-
-            this.DetermineState();
-        }
     }
 }
